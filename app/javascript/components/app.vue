06e6231d9693d48ed5dff748f5fd5f23fb64b860
Tony Williams <92tonywills@gmail.com>
2020-08-25 23:44:49 +0100

    update from end client to fetch pokemon from api

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
     <h1>Is It a Pokemon?</h1>
-    <p>{{ pokemon }}</p>
+    <p>{{ pokemon ? pokemon.name : '...' }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: function() {
     return {
-      pokemon: "Hello Vue!"
+      pokemon: null
     }
+  },
+  created: async function() {
+    let response = await axios.get('/pokemon')
+    this.pokemon = response.data
   }
 }
 </script>
