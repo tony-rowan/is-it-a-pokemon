87ca3ed85544e4fc8970f7e0db05394824b68652
Tony Williams <92tonywills@gmail.com>
2020-08-25 23:17:00 +0100

    initial vue setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,28 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 
+import Vue from 'vue'
+import App from '../components/app.vue'
+import TurbolinksAdapter from 'vue-turbolinks'
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  var element = document.getElementById("app")
+  if (element != null) {
+    var app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+    })
+  }
+})
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     render: h => h(App)
+//   }).$mount()
+//   document.body.appendChild(app.$el)
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+//   console.log(app)
+// })
