904221551f24c616938d2433e109a44957b0b39d
Tony Rowan <trowan812@gmail.com>
2020-10-24 19:25:40 +0100

    Replace javascript app test with rails system test

--- test/system/application_test.rb
+++ test/system/application_test.rb
@@ -1,13 +1,37 @@
 require "application_system_test_case"
 
 class ApplicationTest < ApplicationSystemTestCase
-  test "visiting the applicaiton" do
+  test "display a question" do
     visit root_url
 
     assert_title "Is It A Pokemon?"
     assert_text "Is It a Pokemon?"
-    assert_text "No"
-    assert_text "Yes"
-    # TODO insert a given pokemon and assert that it is displayed  
+    assert_button "No"
+    assert_button "Yes"
+    # TODO insert a given pokemon and assert that it is displayed
+  end
+
+  test "answering a question correctly" do
+    visit root_url
+    click_button "Yes"
+
+    assert_text "Correct!"
+  end
+
+  test "answering a question incorrectly" do
+    visit root_url
+    click_button "No"
+
+    assert_text "Wrong!"
+  end
+
+  test "playing again" do
+    visit root_url
+    click_button "No"
+    click_button "Play Again"
+
+    assert_text "Is It a Pokemon?"
+    assert_button "No"
+    assert_button "Yes"
   end
 end
