2f4b6efddfacc4eebc560622791521bdb4378a9f
Tony Rowan <tony-rowan@cookpad.com>
2022-06-16 14:09:57 +0100

    Add keyboard shortcuts for the buttons

---
+++ app/javascript/controllers/keyboard_shortcut_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    key: String
+  }
+
+  fire(event) {
+    if (event.key == this.keyValue) {
+      this.element.click()
+    }
+  }
+}
